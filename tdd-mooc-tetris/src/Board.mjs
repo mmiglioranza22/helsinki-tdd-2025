810a777f825e5d1e01f3828a19ad397bfaa903ce
Manuel Miglioranza <mmiglioranza22@gmail.com>
2025-03-30 20:48:47 -0300

    drop and tick test pass

--- tdd-mooc-tetris/src/Board.mjs
+++ tdd-mooc-tetris/src/Board.mjs
@@ -1,13 +1,22 @@
 export class Board {
   width;
   height;
+  shape;
 
   constructor(width, height) {
     this.width = width;
     this.height = height;
+    this.shape = "...\n...\n...\n";
+  }
+  drop() {
+    this.shape = ".X.\n...\n...\n";
+  }
+
+  tick() {
+    this.shape = "...\n.X.\n...\n";
   }
 
   toString() {
-    return "...\n...\n...\n";
+    return this.shape;
   }
 }
