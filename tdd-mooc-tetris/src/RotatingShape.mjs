e63d348134da3045ecc05c8d6ead0637b4bac551
Manuel Miglioranza <mmiglioranza22@gmail.com>
2025-04-01 20:47:26 -0300

    can be rotated right/clockwise

--- tdd-mooc-tetris/src/RotatingShape.mjs
+++ tdd-mooc-tetris/src/RotatingShape.mjs
@@ -8,6 +8,33 @@ class Shape {
   toString() {
     return this.shape;
   }
+
+  rotateRight() {
+    let final = [];
+    this.shape = this.shape.split("\n");
+    this.shape.pop();
+
+    const multiplier = this.shape[0].length; // 3, 5, etc
+
+    for (let i = 0; i < multiplier; i++) {
+      final[i] = new Array(multiplier);
+    }
+
+    this.shape = this.shape.reverse();
+    for (let i = multiplier - 1; i >= 0; i--) {
+      for (let j = multiplier - 1; j >= 0; j--) {
+        final[j][i] = this.shape[i][j];
+      }
+    }
+
+    for (let i = 0; i < final.length; i++) {
+      final[i] = final[i].join("");
+    }
+
+    final = final.join("\n") + "\n";
+
+    return final;
+  }
 }
 
 export class RotatingShape {
