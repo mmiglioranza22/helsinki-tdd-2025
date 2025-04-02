2e409cd0c1a2ed1c29799aa7f0c15960f36aacd8
Manuel Miglioranza <mmiglioranza22@gmail.com>
2025-04-02 10:27:27 -0300

    can be rotated left/counter-clockwise

--- tdd-mooc-tetris/src/RotatingShape.mjs
+++ tdd-mooc-tetris/src/RotatingShape.mjs
@@ -33,6 +33,36 @@ class Shape {
 
     final = final.join("\n") + "\n";
 
+    this.shape = final;
+    return final;
+  }
+
+  rotateLeft() {
+    let final = [];
+    this.shape = this.shape.split("\n");
+    this.shape.pop();
+
+    const multiplier = this.shape[0].length; // 3, 5, etc
+
+    for (let i = 0; i < multiplier; i++) {
+      final[i] = new Array(multiplier);
+    }
+
+    // this.shape = this.shape.reverse();
+    for (let i = 0; i < multiplier; i++) {
+      for (let j = multiplier - 1; j >= 0; j--) {
+        console.log(this.shape[i][j]);
+        final[i][multiplier - 1 - j] = this.shape[i][j];
+      }
+    }
+
+    final = final.reverse();
+    for (let i = 0; i < final.length; i++) {
+      final[i] = final[i].join("");
+    }
+    final = final.join("\n") + "\n";
+
+    this.shape = final;
     return final;
   }
 }
